e79f6b2ce384c1c4d9a853eabb0f4f3f070ad609
Marlon López <malm.gm@gmail.com>
2024-02-13 15:00:54 -0500

    feat(ta_sample): Added copyright headers, fixed language selector for languages with country variant

--- ta_sample/src/main/kotlin/com/github/marlonlom/timeago/sample/utils/SupportedLanguageSelector.kt
+++ ta_sample/src/main/kotlin/com/github/marlonlom/timeago/sample/utils/SupportedLanguageSelector.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2024, marlonlom
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.github.marlonlom.timeago.sample.utils
 
 import com.github.marlonlom.utilities.timeago.TimeAgoMessages
@@ -30,7 +47,7 @@ object SupportedLanguageSelector {
     "pt" to "Portuguese",
     "tr" to "Turkish",
     "zh" to "Chinese",
-    "zh_TW" to "Chinese/Taiwan",
+    "zh-TW" to "Chinese/Taiwan",
   )
 
   /** Available supported languages text. */
@@ -53,7 +70,8 @@ object SupportedLanguageSelector {
       .withLocale(Locale.forLanguageTag(lang))
       .build()
 
-    else -> TimeAgoMessages.Builder().defaultLocale().build()
+    else -> TimeAgoMessages.Builder()
+      .defaultLocale().build()
   }
 
   /**
