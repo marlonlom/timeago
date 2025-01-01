8d6f6382521c9a9994c9ecad422b48a8ec3a5d55
Marlon López <malm.gm@gmail.com>
2024-12-31 23:47:01 -0500

    feat: Update build.gradle.kts for android sample module

--- ta_sample/build.gradle.kts
+++ ta_sample/build.gradle.kts
@@ -23,12 +23,12 @@ plugins {
 
 android {
   namespace = "com.github.marlonlom.timeago.sample"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.github.marlonlom.timeago.sample"
     minSdk = 24
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0.0"
 
