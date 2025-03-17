0c95bd1799c946128241eac19469ccb529d5e7a1
Marlon López <malm.gm@gmail.com>
2025-03-16 20:38:41 -0500

    Configure spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,4 +35,58 @@ plugins {
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.spotless) apply false
 }
+
+/**
+ * Spotless plugin configuration
+ */
+subprojects {
+  apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+  configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+      target("**/*.kt")
+      targetExclude("${layout.buildDirectory}/**/*.kt")
+      ktlint().editorConfigOverride(
+        mapOf(
+          "android" to "true",
+          "charset" to "utf-8",
+          "end_of_line" to "lf",
+          "indent_style" to "space",
+          "indent_size" to 2,
+          "insert_final_newline" to true,
+          "max_line_length" to 120,
+          "trim_trailing_whitespace" to true,
+          "ij_continuation_indent_size" to 2,
+          "ij_kotlin_name_count_to_use_star_import" to 999,
+          "ij_kotlin_name_count_to_use_star_import_for_members" to 999,
+          "ktlint_function_naming_ignore_when_annotated_with" to "Composable"
+        ),
+      )
+      trimTrailingWhitespace()
+      leadingTabsToSpaces()
+      endWithNewline()
+      licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+    }
+
+    kotlinGradle {
+      target("**/*.kts")
+      targetExclude("**/build/**/*.kts")
+      ktlint()
+      trimTrailingWhitespace()
+      leadingTabsToSpaces()
+      endWithNewline()
+      licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+    }
+
+    format("xml") {
+      target("**/*.xml")
+      targetExclude("**/build/**/*.xml")
+      trimTrailingWhitespace()
+      leadingTabsToSpaces()
+      endWithNewline()
+      licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+    }
+  }
+}
+
