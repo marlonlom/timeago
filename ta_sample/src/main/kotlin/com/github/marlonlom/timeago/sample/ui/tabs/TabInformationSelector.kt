0b71119db3774b49748c6848883fd6ebaadf1072
Marlon López <malm.gm@gmail.com>
2025-10-05 12:07:30 -0500

    refactor(ta_sample): change local context to local resources composable function for retrieving resources

--- ta_sample/src/main/kotlin/com/github/marlonlom/timeago/sample/ui/tabs/TabInformationSelector.kt
+++ ta_sample/src/main/kotlin/com/github/marlonlom/timeago/sample/ui/tabs/TabInformationSelector.kt
@@ -16,7 +16,7 @@
  */
 package com.github.marlonlom.timeago.sample.ui.tabs
 
-import android.content.Context
+import android.content.res.Resources
 import com.github.marlonlom.timeago.sample.R
 import com.github.marlonlom.timeago.sample.utils.CalendarSampleDataUtil
 import com.github.marlonlom.timeago.sample.utils.SupportedLanguageSelector
@@ -35,7 +35,7 @@ object TabInformationSelector {
   /**
    * Prepare usage dates information.
    *
-   * @param context Application context.
+   * @param resources Application context.
    * @param tabDetailTpl Tab detail text template.
    * @param isPast True/False if past times handling.
    * @param sdf Simple date format.
@@ -47,7 +47,7 @@ object TabInformationSelector {
    * @return Dates usage information as text.
    */
   fun prepareUsageDatesInformation(
-    context: Context,
+    resources: Resources,
     tabDetailTpl: String,
     isPast: Boolean,
     sdf: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault()),
@@ -62,7 +62,7 @@ object TabInformationSelector {
     val timeAgoMessages = SupportedLanguageSelector.getTimeAgoMessagesByLang(languageCode)
 
     val builder1 = StringBuilder()
-    builder1.append(getHeadingText(context, languageCode, isPast))
+    builder1.append(getHeadingText(resources, languageCode, isPast))
 
     for (item in arrayList) {
       val calendar = Calendar.getInstance()
@@ -73,24 +73,24 @@ object TabInformationSelector {
     }
 
     val resId = if (isPast) R.string.tabbed_main_detail_from else R.string.tabbed_main_detail_until
-    return context.getString(resId, builder1.toString())
+    return resources.getString(resId, builder1.toString())
   }
 
   /**
    * Returns heading text for elapsed and remaining tab contents.
    *
-   * @param context Application context.
+   * @param resources Application context.
    * @param languageCode Provided language code.
    * @param isPast True/False if past times handling.
    *
    * @return Generated heading text.
    */
-  private fun getHeadingText(context: Context, languageCode: String, isPast: Boolean): String {
+  private fun getHeadingText(resources: Resources, languageCode: String, isPast: Boolean): String {
     val timelineText = when {
-      isPast -> context.getString(R.string.tabbed_main_heading_past)
-      else -> context.getString(R.string.tabbed_main_heading_future)
+      isPast -> resources.getString(R.string.tabbed_main_heading_past)
+      else -> resources.getString(R.string.tabbed_main_heading_future)
     }
     val languageText = SupportedLanguageSelector.getLanguageTextFor(languageCode)
-    return context.getString(R.string.tabbed_main_heading, timelineText, languageText)
+    return resources.getString(R.string.tabbed_main_heading, timelineText, languageText)
   }
 }
