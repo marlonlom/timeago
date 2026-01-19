b4a9655a560d8499d678f64365819c6d3d7f3229
Marlon López <malm.gm@gmail.com>
2026-01-19 13:22:46 -0500

    Actualizar build.gradle.kts

--- ta_sample/build.gradle.kts
+++ ta_sample/build.gradle.kts
@@ -20,7 +20,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
   alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
 }
 
